e3c5d5b4c689188fdd2430569c413301773ea84f
Steven Yerxa <syerxa@pingidentity.com>
2019-01-24 15:10:00 -0400

    removed local connection to mysql

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from routes.routes import api
 def create_app():
     app = Flask(__name__)
     # Edit following line to configure the location of your mysql db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:2Federate@localhost/dhdemo'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<username>:<password>@localhost/<database>'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     cache.init_app(app)
